ce9fb07a5357e4b91f6cde6a0a445f8388560749
waterminn <tnals08019@gmail.com>
2023-05-18 13:36:51 +0900

    확장함수 추가

---
+++ app/src/main/java/com/sumin/android/carrot_aos/util/extension/ViewExt.kt
@@ -0,0 +1,38 @@
+package com.sumin.android.carrot_aos.util.extension
+
+import android.content.Context
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.google.android.material.snackbar.Snackbar
+
+// 화면 전체
+fun View.showSnackbar(
+    message: String,
+    duration: Int = Snackbar.LENGTH_SHORT,
+) {
+    Snackbar
+        .make(this, message, duration)
+        .show()
+}
+
+// 해당 뷰 위로 올릴 때
+fun View.showSnackbar(
+    message: String,
+    anchor: Int,
+    duration: Int = Snackbar.LENGTH_SHORT,
+) {
+    Snackbar
+        .make(this, message, duration)
+        .setAnchorView(anchor)
+        .show()
+}
+
+fun View.hideKeyboard() {
+    try {
+        val imm = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        imm?.hideSoftInputFromWindow(windowToken, 0)
+    } catch (e: Exception) {
+        Log.e("error", e.message.toString())
+    }
+}
