7a58c9e7dca5496dae89a9d19cebb8951d019708
waterminn <tnals08019@gmail.com>
2023-05-18 13:34:09 +0900

    activity, fragment 예시

--- app/src/main/java/com/sumin/android/carrot_aos/presentation/MainActivity.kt
+++ app/src/main/java/com/sumin/android/carrot_aos/presentation/MainActivity.kt
@@ -1,11 +1,11 @@
-package com.sumin.android.carrot_aos
+package com.sumin.android.carrot_aos.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sumin.android.carrot_aos.R
+import com.sumin.android.carrot_aos.databinding.ActivityMainBinding
+import com.sumin.android.carrot_aos.presentation.base.BindingActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+
 }
---
+++ app/src/main/java/com/sumin/android/carrot_aos/presentation/home/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.sumin.android.carrot_aos.presentation.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sumin.android.carrot_aos.R
+import com.sumin.android.carrot_aos.databinding.FragmentHomeBinding
+import com.sumin.android.carrot_aos.presentation.base.BindingFragment
+
+class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+}
