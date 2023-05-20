645453e0138616012ee33c575950b083d498883a
l2zh <dlwnsgml0109@naver.com>
2023-05-21 01:06:04 +0900

    {#4} [ui] sale 뷰 리사이클러뷰 아이템

---
+++ app/src/main/java/com/sumin/android/carrot_aos/presentation/sale/SaleActivity.kt
@@ -0,0 +1,12 @@
+package com.sumin.android.carrot_aos.presentation.sale
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sumin.android.carrot_aos.R
+
+class SaleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sale)
+    }
+}
