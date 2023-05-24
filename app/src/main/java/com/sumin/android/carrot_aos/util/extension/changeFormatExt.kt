fa1db972dcd14a083117f34158c4f43a0c0a035f
jihyunniiii <jita1125@naver.com>
2023-05-24 17:09:05 +0900

    #14 [feat] 화폐 콤마 표시 format 변경 확장함수 추가

--- app/src/main/java/com/sumin/android/carrot_aos/util/extension/changeFormatExt.kt
+++ app/src/main/java/com/sumin/android/carrot_aos/util/extension/changeFormatExt.kt
@@ -1,5 +1,6 @@
 package com.sumin.android.carrot_aos.util.extension
 
+import java.text.DecimalFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.Locale
@@ -10,4 +11,10 @@ fun changeTimeFormat(inputTime: String): String {
     val originTimeFormat = LocalDateTime.parse(inputTime, originFormatter)
 
     return changeFormatter.format(originTimeFormat)
+}
+
+fun changePriceFormat(inputPrice: Int): String {
+    val formatter = DecimalFormat("#,###")
+
+    return formatter.format(inputPrice)
 }
