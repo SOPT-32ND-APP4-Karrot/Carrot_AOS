1e41c62c3244a3803094fb36a713a7f4855ff264
jihyunniiii <jita1125@naver.com>
2023-05-26 07:36:49 +0900

    #14 [ui] ReviewActivity 리사이클러뷰 기능 구현

--- app/src/main/java/com/sumin/android/carrot_aos/presentation/review/ReviewActivity.kt
+++ app/src/main/java/com/sumin/android/carrot_aos/presentation/review/ReviewActivity.kt
@@ -1,13 +1,16 @@
 package com.sumin.android.carrot_aos.presentation.review
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.sumin.android.carrot_aos.data.model.response.ReviewResponse
 import com.sumin.android.carrot_aos.databinding.ActivityReviewBinding
 
 class ReviewActivity : AppCompatActivity() {
     private lateinit var binding: ActivityReviewBinding
     private val viewModel by viewModels<ReviewViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityReviewBinding.inflate(layoutInflater)
@@ -15,5 +18,28 @@ class ReviewActivity : AppCompatActivity() {
 
         //viewModel.connectReviewOnline(intent.getStringExtra("reviewId")?.toLong() ?: 0)
         viewModel.connectReviewOnline(2)
+        setReviewResultObserver()
+    }
+
+    private fun setReviewResultObserver() {
+        viewModel.reviewResult.observe(this) { reviewResponse ->
+            connectReviewAdapter(reviewResponse)
+        }
+    }
+
+    private fun connectReviewAdapter(reviewResponse: ReviewResponse?) {
+        val reviewAdapter = ReviewAdapter(this)
+        val reviewList = mutableListOf<ReviewResponse.Review>()
+
+        if (reviewResponse != null) {
+            reviewAdapter.setSellerInfo(reviewResponse.data.receiverReview.writer)
+            with(reviewResponse.data) {
+                reviewList.add(senderReview)
+                reviewList.add(receiverReview)
+                reviewList.add(receiverReview)
+            }
+            reviewAdapter.submitList(reviewList)
+            binding.rvReviewCard.adapter = reviewAdapter
+        }
     }
 }
---
+++ app/src/main/java/com/sumin/android/carrot_aos/presentation/review/ReviewAdapter.kt
@@ -0,0 +1,90 @@
+package com.sumin.android.carrot_aos.presentation.review
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.sumin.android.carrot_aos.R
+import com.sumin.android.carrot_aos.data.model.response.ReviewResponse
+import com.sumin.android.carrot_aos.databinding.ItemReviewButtonBinding
+import com.sumin.android.carrot_aos.databinding.ItemReviewCardBinding
+import com.sumin.android.carrot_aos.util.extension.ItemDiffCallback
+
+class ReviewAdapter(context: Context) :
+    ListAdapter<ReviewResponse.Review, RecyclerView.ViewHolder>(
+        ItemDiffCallback<ReviewResponse.Review>(
+            onContentsTheSame = { old, new -> old == new },
+            onItemsTheSame = { old, new -> old == new }
+        )
+    ) {
+    private val inflater by lazy { LayoutInflater.from(context) }
+    private lateinit var sellerInfo: ReviewResponse.Writer
+
+    class ReviewCardViewHolder(
+        private val binding: ItemReviewCardBinding,
+        private val seller: ReviewResponse.Writer
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(data: ReviewResponse.Review, viewType: Int) {
+            when (viewType) {
+                VIEW_TYPE_MINT -> binding.tvItemReviewTitle.text =
+                    "${seller.nickname}님에게\n따뜻한 후기를 보냈어요."
+
+                VIEW_TYPE_LIGHT_YELLOW -> {
+                    with(binding) {
+                        tvItemReviewTitle.text = "${seller.nickname}님이 보낸\n 따뜻한 후기가 도착했어요."
+                        ivItemReviewCardImg.setImageResource(R.drawable.img_review_card_lightyellow)
+                        layoutItemReviewCard.setBackgroundResource(R.drawable.shape_lightyellow_fill_20_rect)
+                    }
+                }
+            }
+        }
+    }
+
+    class MyMannerViewHolder(
+        private val binding: ItemReviewButtonBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            VIEW_TYPE_BUTTON -> {
+                val binding = ItemReviewButtonBinding.inflate(inflater, parent, false)
+                MyMannerViewHolder(binding)
+            }
+
+            else -> {
+                val binding = ItemReviewCardBinding.inflate(inflater, parent, false)
+                ReviewCardViewHolder(binding, sellerInfo)
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is ReviewCardViewHolder -> holder.onBind(
+                currentList[position],
+                getItemViewType(position)
+            )
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (position) {
+            0 -> VIEW_TYPE_MINT
+            currentList.size - 1 -> VIEW_TYPE_BUTTON
+            else -> VIEW_TYPE_LIGHT_YELLOW
+        }
+    }
+
+    fun setSellerInfo(seller: ReviewResponse.Writer) {
+        this.sellerInfo = seller
+    }
+
+    companion object {
+        const val VIEW_TYPE_MINT = 0
+        const val VIEW_TYPE_LIGHT_YELLOW = 1
+        const val VIEW_TYPE_BUTTON = 2
+    }
+}
